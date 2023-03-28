fc8aa093b4fb9881451a175e626dfe7417a24c60
valeriofilareti <valerio.filareti@gmail.com>
2023-03-28 17:53:28 +0200

    [fix] even-odd function

--- pari-dispari/script.js
+++ pari-dispari/script.js
@@ -6,20 +6,32 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
+
+// Scelte utente
 const userNum = 3; //prompt('scegli un numero da 1 a 5')
-const userChoise = 'even' //prompt('scegli pari o dispari')
+console.log('tuo numero --->',userNum);
+const userChoise = 'pari' //prompt('scegli pari o dispari')
+console.log('tua scelta --->', userChoise);
 
-
+// Numero casuale pc
 const pcNum = generateRandomNumber();
+console.log('numero pc --->',pcNum);
 
-
+// Somma dei due numeri 
 const finalResult = parseInt(userNum) + parseInt(pcNum);
+console.log('somma --->',finalResult);
+
+// Risultato finale
+const esito = evenOdd(finalResult)
+
+if (esito === userChoise){
+  console.log('hai vinto');
+} else {
+  console.log('hai perso');
+}
 
 
-const abc = evenOdd(finalResult)
-console.log(abc);
-
-
+// Funzioni
 
 function generateRandomNumber () {
   const randomNumber = Math.floor(Math.random()* 5) + 1;
@@ -28,10 +40,10 @@ function generateRandomNumber () {
 
 function evenOdd(final) {
   let result;
-  if(final % 2 === 0){
-    const result = 'odd';
+  if(final % 2 == 0){
+    result = 'pari';
   }else{
-    const result = 'even'
+    result = 'dispari'
   }
   return  result;
 }
