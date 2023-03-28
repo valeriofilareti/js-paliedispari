18e5f8940bec80c7fb665e6bac72528fde4f8b1a
valeriofilareti <valerio.filareti@gmail.com>
2023-03-28 16:52:19 +0200

    first commit

--- script.js
+++ script.js
@@ -4,24 +4,6 @@ Creare una funzione per capire se la parola inserita è palindroma
 */
 
 
-// const arr = ['a', 'b', 'c']
-// const arrReverse = arr.reverse()
-
-// console.log(arrReverse);
-
-
-
-// const reversedWord = reverseWord('ciao')
-
-// console.log(reversedWord);
-// function reverse(word){
-  //   const arrWord = word.split('');
-  //   const revWord = arrWord.reverse()
-  //   if(arrWord === revWord){
-    
-    //   }
-    // }
-
 const userWord = prompt('parola')
 
 const reversedWord = rev(userWord)
